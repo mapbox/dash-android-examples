c8588b69f7f0f59a960883b4fbc73d534dbf396c
Aleksandr Zatsepin <aleksandr.zatsepin@mapbox.com>
2023-12-22 21:46:26 +0300

    Migrate to Beta.20 (Dash -> Navigation UX) (#18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ android {
 }
 
 dependencies {
-    implementation("com.mapbox.dash:android:1.0.0-beta.19")
-    implementation("com.mapbox.dash:search-api:1.0.0-beta.19")
+    implementation("com.mapbox.navigationux:android:1.0.0-beta.20")
+    implementation("com.mapbox.navigationux:search-api:1.0.0-beta.20")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
