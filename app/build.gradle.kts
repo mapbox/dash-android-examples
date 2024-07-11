7fb32e6c2f26e3382991bfd51e87e6e845fd031e
Yury Kanetski <ringerjk@gmail.com>
2024-07-11 10:11:21 +0200

    Release signing config: set Debug signing config (#25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
