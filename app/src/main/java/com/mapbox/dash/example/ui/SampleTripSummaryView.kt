94b4bb54fec1dd992687da8d906e82a087990a56
Yuri <yuryybk@users.noreply.github.com>
2024-11-19 12:04:41 +0300

    Add edit button to custom trip summary view (#96)

--- app/src/main/java/com/mapbox/dash/example/ui/SampleTripSummaryView.kt
+++ app/src/main/java/com/mapbox/dash/example/ui/SampleTripSummaryView.kt
@@ -1,16 +1,18 @@
 package com.mapbox.dash.example.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -19,9 +21,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mapbox.dash.compose.component.Button1
 import com.mapbox.dash.destination.preview.domain.model.TripOverviewItem
 import com.mapbox.dash.driver.R
 import com.mapbox.dash.driver.presentation.end.TripSummaryUiState
@@ -43,6 +45,7 @@ fun SampleTripSummaryView(
 
     val weatherForecast = weatherViewModel.weatherForecastOnDestination.collectAsState(null).value
 
+    val defaultMargin = dimensionResource(R.dimen.default_margin)
     val model = tripSummaryUiState.tripSummaryModel
     Column(
         modifier = modifier
@@ -55,41 +58,70 @@ fun SampleTripSummaryView(
     ) {
 
         SampleTripOverviewItems(
-            modifier = Modifier.padding(dimensionResource(R.dimen.default_margin)),
+            modifier = Modifier.padding(start = defaultMargin, end = defaultMargin, top = defaultMargin),
             items = tripSummaryUiState.tripOverviewItems,
             onWaypointClick = tripSummaryUiState.onWaypointClick,
             onEndOfChargeClick = tripSummaryUiState.onOpenSearchForChargeClick,
             weatherForecast = weatherForecast,
         )
-        Box {
 
-            Button1(
-                text = stringResource(id = R.string.dash_end_active_guidance_end_route),
-                color = AppTheme.colors.textColor.inverted,
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(dimensionResource(R.dimen.default_margin))
-                    .height(dimensionResource(id = R.dimen.button_height))
-                    .clip(shape = AppTheme.shapes.actionButtonBackground)
-                    .background(AppTheme.colors.buttonColors.red)
-                    .clickable(onClick = tripSummaryUiState.onEndActiveGuidanceClick)
-                    .padding(dimensionResource(R.dimen.default_margin))
-                    .wrapContentSize(),
+        SampleEtaView(
+            modifier = Modifier.fillMaxWidth(),
+            arrivalTime = model.legArrivalTime,
+            remainingDistance = model.legDistanceRemaining,
+            remainingTime = model.legTimeRemaining,
+            isOffline = model.isOffline,
+            stateOfCharge = model.legStateOfCharge,
+            traveledToRemainingRatio = model.traveledToRemainingRatio,
+            trafficGradientStops = model.trafficGradientStops.toTypedArray(),
+            waypointsData = model.waypointsData,
+        )
+
+        Row(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = defaultMargin,
+                        end = defaultMargin,
+                        bottom = defaultMargin
+                    ),
+            horizontalArrangement = Arrangement.spacedBy(defaultMargin)
+        ) {
+
+            SampleTripSummaryActionButton(
+                textId = R.string.dash_end_active_guidance_edit_trip,
+                backgroundColor = AppTheme.colors.buttonColors.primary,
+                onClick = tripSummaryUiState.onEditTripClick
             )
 
-            SampleEtaView(
-                modifier = Modifier.fillMaxWidth().align(Alignment.TopStart),
-                arrivalTime = model.legArrivalTime,
-                remainingDistance = model.legDistanceRemaining,
-                remainingTime = model.legTimeRemaining,
-                isOffline = model.isOffline,
-                stateOfCharge = model.legStateOfCharge,
-                traveledToRemainingRatio = model.traveledToRemainingRatio,
-                trafficGradientStops = model.trafficGradientStops.toTypedArray(),
-                waypointsData = model.waypointsData,
+            SampleTripSummaryActionButton(
+                textId = R.string.dash_end_active_guidance_end_route,
+                backgroundColor = AppTheme.colors.buttonColors.red,
+                onClick = tripSummaryUiState.onEndActiveGuidanceClick
             )
+
         }
     }
+}
+
+@Composable
+private fun RowScope.SampleTripSummaryActionButton(
+    @StringRes textId: Int,
+    backgroundColor: Color,
+    onClick: () -> Unit,
+) {
+    Text(
+        text = stringResource(id = textId),
+        color = AppTheme.colors.textColor.inverted,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .weight(1f)
+            .clip(shape = AppTheme.shapes.actionButtonBackground)
+            .background(backgroundColor)
+            .clickable(onClick = onClick)
+            .padding(dimensionResource(R.dimen.default_margin)),
+        )
 }
 
 @Composable
