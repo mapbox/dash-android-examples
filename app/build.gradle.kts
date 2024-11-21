75035d395adb99f83a8cc0a0ae0ef50f5d37204f
Vitaly Zayankovsky <vitaliy.zayankovskiy@mapbox.com>
2024-11-21 11:55:33 +0300

    add custom search panel example (#99)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.material:material")
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
