6ba0143cce52e2ee4d7d340448fd973f6c48f55f
siarhei.mishchanka <vitaminpsg@gmail.com>
2024-12-16 12:31:32 +0300

    - Add private sdk flavor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
         create("CustomCluster") {
             dimension = "devMenuDefaults"
         }
+        create("PrivateSdk") {
+            dimension = "devMenuDefaults"
+        }
     }
 
     buildTypes {
@@ -100,4 +103,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+}
+
+fun DependencyHandler.privateSdk(dependencyNotation: String) {
+    add("PrivateSdkImplementation", dependencyNotation)
 }
