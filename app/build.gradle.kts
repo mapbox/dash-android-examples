df6e0e745372f92f01fdbc11f09872937f6f95a3
alexzatsepin <alexander.zatsepin@mapbox.com>
2025-04-09 23:00:18 +0300

    Migrate to RC.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ android {
 }
 
 dependencies {
-    val uxfVersion = "1.0.0-rc.3.1"
+    val uxfVersion = "1.0.0-rc.3.2"
     implementation("com.mapbox.navigationux:android:$uxfVersion")
     implementation("com.mapbox.navigationux:cluster:$uxfVersion")
     implementation("com.mapbox.navigationux:weather-api:$uxfVersion")
