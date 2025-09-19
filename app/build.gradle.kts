88212f8720f5a2e8580ec051ba0070903554e7a1
Vitaly Zayankovsky <vitaliy.zayankovskiy@mapbox.com>
2025-09-19 15:19:10 +0300

    bump uxf to 1.3.0 (#169)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.7"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += setOf(
                 "dash-sdk.properties",
@@ -84,7 +84,7 @@ android {
 }
 
 dependencies {
-    val uxfVersion = "1.3.0-rc.1"
+    val uxfVersion = "1.3.0"
     implementation("com.mapbox.navigationux:android:$uxfVersion")
     implementation("com.mapbox.navigationux:cluster:$uxfVersion")
     implementation("com.mapbox.navigationux:weather-api:$uxfVersion")
